50ed1409c08d6a78b8950bcfa8b52e1d3d5a6c6e
aditya3194 <adityamahajan6040@gmail.com>
2023-10-26 15:25:06 +0530

    s

--- streamlit_app.py
+++ streamlit_app.py
@@ -127,51 +127,51 @@ st.text("This is some text.")
 
 
 
-# Store LLM generated responses
-if "messages" not in st.session_state.keys():
-    st.session_state.messages = [{"role": "assistant", "content": "नमस्ते! कैसे मदद कर सकता हूँ?"}]
+# # Store LLM generated responses
+# if "messages" not in st.session_state.keys():
+#     st.session_state.messages = [{"role": "assistant", "content": "नमस्ते! कैसे मदद कर सकता हूँ?"}]
 
-# Display chat messages
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.write(message["content"])
-
-
-# User-provided prompt
-if prompt := st.chat_input():
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.write(prompt)
+# # Display chat messages
+# for message in st.session_state.messages:
+#     with st.chat_message(message["role"]):
+#         st.write(message["content"])
 
 
+# # User-provided prompt
+# if prompt := st.chat_input():
+#     st.session_state.messages.append({"role": "user", "content": prompt})
+#     with st.chat_message("user"):
+#         st.write(prompt)
 
 
 
-if st.session_state.messages[-1]["role"] != "assistant":
-    with st.spinner("Thinking..."):
-        time.sleep(2)
-        user_input = prompt.lower()
-        message = []
-        if user_input in responses:
-            st.chat_message("assistant:")
-            st.write(responses[user_input])
-            message = {"role": "assistant", "content": responses[user_input]}
-        else:
-            st.chat_message("assistant:")
-            st.write("I'm sorry, I don't have the information you requested. Please try asking something else.")
-            message = {"role": "assistant", "content": "No Information about it"}
-    st.session_state.messages.append(message)
 
 
-# Handle user input and display responses
-# if st.button("Send"):
-#     user_input = user_input.lower()
-#     if user_input in responses:
-#         st.text("Chatbot:")
-#         st.write(responses[user_input])
-#     else:
-#         st.text("Chatbot:")
-#         st.write("I'm sorry, I don't have the information you requested. Please try asking something else.")
+# if st.session_state.messages[-1]["role"] != "assistant":
+#     with st.spinner("Thinking..."):
+#         time.sleep(2)
+#         user_input = prompt.lower()
+#         message = []
+#         if user_input in responses:
+#             st.chat_message("assistant:")
+#             st.write(responses[user_input])
+#             message = {"role": "assistant", "content": responses[user_input]}
+#         else:
+#             st.chat_message("assistant:")
+#             st.write("I'm sorry, I don't have the information you requested. Please try asking something else.")
+#             message = {"role": "assistant", "content": "No Information about it"}
+#     st.session_state.messages.append(message)
+
+
+# # Handle user input and display responses
+# # if st.button("Send"):
+# #     user_input = user_input.lower()
+# #     if user_input in responses:
+# #         st.text("Chatbot:")
+# #         st.write(responses[user_input])
+# #     else:
+# #         st.text("Chatbot:")
+# #         st.write("I'm sorry, I don't have the information you requested. Please try asking something else.")
 
 # Instructions for the user
 st.sidebar.header("Instructions")

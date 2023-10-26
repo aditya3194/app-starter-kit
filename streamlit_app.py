3fe1ad603b37e2f14fde4e6c4f8bccc72043c822
aditya3194 <adityamahajan6040@gmail.com>
2023-10-26 15:25:53 +0530

    s

--- streamlit_app.py
+++ streamlit_app.py
@@ -74,7 +74,7 @@ for file in glob.glob(pdf_path):
     item = extract_text_from_pdf(file)
     pdfItems.extend(item)
 
-st.text(pdfItems)
+st.text(balanceSheetItems)
 
 # #databases
 # balanceSheetDB = FAISS.from_documents(balanceSheetItems, CohereEmbeddings())
